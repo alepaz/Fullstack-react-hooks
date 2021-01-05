a61e2bfa368c25922a73207b98075ba32b0aecdd
Alejandro Paz <alepazte@hotmail.com>
2021-01-04 23:54:10 -0600

    console test using the reducer

--- redux/chat_simple/src/App.js
+++ redux/chat_simple/src/App.js
@@ -26,3 +26,25 @@ function reducer(state, action) {
 const initialState = { messages: [] };
 
 const store = createStore(reducer, initialState);
+
+const addMessageAction1 = {
+  type: "ADD_MESSAGE",
+  messages: "How does it look, Neil?",
+};
+
+store.dispatch(addMessageAction1);
+
+const stateV1 = store.getState();
+
+const addMessageAction2 = {
+  type: "ADD_MESSAGE",
+  messages: "Looking good",
+};
+
+store.dispatch(addMessageAction2);
+const stateV2 = store.getState();
+
+console.log("State v1:");
+console.log(stateV1);
+console.log("State v2:");
+console.log(stateV2);
