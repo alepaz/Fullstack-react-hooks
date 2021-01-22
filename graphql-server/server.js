e32ee21fe9369a59b6c887fe75e13c9f288150a1
Alejandro Paz <alepazte@hotmail.com>
2021-01-21 22:37:33 -0600

    feat: add graphql resolver

---
+++ graphql-server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+
+const app = express();
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  description: "The root query",
+  fields: {
+    viewer: {
+      type: GraphQLString,
+      resolve() {
+        return "viewer!";
+      },
+    },
+  },
+});
+
+const Schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: Schema,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log({ running: true });
+});
