f2205ca362290ab51ba6737f1330f8f2f4e15d05
Alejandro Paz <alepazte@hotmail.com>
2021-01-22 00:56:38 -0600

    feat: add mutation for schema

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import graphqlHTTP from "express-graphql";
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLID,
+} from "graphql";
 
 const app = express();
 
@@ -15,10 +21,45 @@ const RootQuery = new GraphQLObjectType({
       },
     },
   },
+  node: {
+    type: GraphQLString,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID),
+      },
+    },
+    resolve(source, args) {
+      return inMemoryStore[args.key];
+    },
+  },
+});
+
+let inMemoryStore = {};
+const RootMutation = new GraphQLObjectType({
+  name: "RootMutation",
+  description: "The root mutation",
+  fields: {
+    setNode: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+        value: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(source, args) {
+        inMemoryStore[args.key] = args.value;
+        return inMemoryStore[args.key];
+      },
+    },
+  },
 });
 
 const Schema = new GraphQLSchema({
   query: RootQuery,
+  mutation: RootMutation,
 });
 
 app.use(
